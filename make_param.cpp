7ef63849001709b886eba92b0c30c68d54071690
yuki0626 <s1240109.uoa@gmail.com>
2019-06-24 00:44:15 +0900

    add

---
+++ make_param.cpp
@@ -0,0 +1,72 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  int Np_min = 10;
+  int Np_max = 100;
+
+  double beta_min = 1;
+  double beta_max = 1;
+
+  double delta_min = 0.2;
+  double delta_max = 1;
+
+  double Delta_p = 0.01;
+
+  int relaxation_steps = 100000;
+  int MC_steps = 5000000;
+
+  double a_min = -3;
+  double a_max = 3;
+
+  double b_min = 0.5;
+  double b_max = 1;
+
+  int N = 50;
+  int nshow = 100000000;
+
+  int index = 0;
+
+  string dir = "./params/";
+  string logdir = "./log/";
+
+  for (int Np = Np_min; Np <= Np_max; Np += 10) {
+
+    for (double beta = beta_min; beta <= beta_max; beta += 0.2) {
+
+      for (double delta = delta_min; delta <= delta_max; delta += 0.1) {
+
+        for (double a = a_min; a <= a_max; a += 0.5) {
+
+          for (double b = a + b_min; b <= a + b_max; b += 0.5) {
+
+            string name = "param" + to_string(index) + ".txt";
+            string filename = dir + name;
+            string logfilename = logdir + "log" + to_string(index) + ".txt";
+            ofstream output(filename);
+
+            output << "Np\t" << Np << endl;
+            output << "beta\t" << beta << endl;
+            output << "delta\t" << delta << endl;
+            output << "Delta_p\t" << Delta_p << endl;
+            output << "relaxation_steps\t" << relaxation_steps << endl;
+            output << "MC_steps\t" << MC_steps << endl;
+            output << "a\t" << a << endl;
+            output << "b\t" << b << endl;
+            output << "N\t" << N << endl;
+            output << "nshow\t" << nshow << endl;
+            output << "logfile\t" << logfilename;
+            cout << filename << endl;
+
+            index += 1;
+          } // b
+        }   // a
+      }     // delta
+    }       // beta
+  }         // Np
+
+  return 0;
+}
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -e "params" ]; then
+    echo "dir params found."
+else
+    echo "make dir params."
+    mkdir params
+fi
+
+if [ -e "log" ]; then
+    echo "dir log found."
+else
+    echo "make dir log."
+    mkdir log
+fi
+
+echo "make params"
+
+echo "g++ make_param.cpp"
+g++ make_param.cpp
+
+./a.out
+
+echo "rm a.out"
+rm a.out
+
+echo "make"
+make
+
